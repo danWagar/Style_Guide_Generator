c6323a548d70121835012dc091d34b229ef655ba
danWagar <dandwagar@gmail.com>
2020-03-28 15:48:43 -0700

    set up color selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { SketchPicker } from 'react-color';
+import React, { useState } from 'react';
+import { SketchPicker, ColorChangeHandler } from 'react-color';
 import Header from './Header/Header';
 import LandingPage from './LandingPage/LandingPage';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('#fff');
+
+  const handleChangeComplete: ColorChangeHandler = col => {
+    setColor(col.hex);
+  };
+
   return (
     <>
       <Header />
       <LandingPage />
-      <SketchPicker />
+      <SketchPicker color={color} onChangeComplete={handleChangeComplete} />
     </>
   );
 }
